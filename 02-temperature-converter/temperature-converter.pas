4b8cd944507416a21613ce470bb09f03922acfb2
Lucas Åström <lucasas@kth.se>
2014-12-31 00:00:29 +0100

    now handles fahrenheit to celcius as well

--- 02-temperature-converter/temperature-converter.pas
+++ 02-temperature-converter/temperature-converter.pas
@@ -5,6 +5,11 @@ Begin
 	CelciusToFahrenheit := Temperature * 1.8 + 32
 End;
 
+Function FahrenheitToCelcius(Temperature:Real) : Real;
+Begin
+	FahrenheitToCelcius := (Temperature - 32) / 1.8
+End;
+
 Var
 Source		: Char;
 Temperature	: Real;
@@ -21,7 +26,13 @@ Begin
 					Temperature := CelciusToFahrenheit(Temperature);
 					write('That is equal to ');
 					write(Temperature:1:2);
-					write(' degrees Fahrenheit.');
+					writeln(' degrees Fahrenheit.');
+				End;
+		'F':	Begin
+					Temperature := FahrenheitToCelcius(Temperature);
+					write('That is equal to ');
+					write(Temperature:1:2);
+					writeln(' degrees Celcius.');
 				End;
 	End;
 End.
