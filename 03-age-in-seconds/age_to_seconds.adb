30261786eaeec375c0fd26632d5d1cfc43082cb1
Lucas Åström <lucasas@kth.se>
2014-12-31 13:57:27 +0100

    use ada's built in calendar arithmetics to get the number of days and seconds
    bonus points for leap seconds

--- 03-age-in-seconds/age_to_seconds.adb
+++ 03-age-in-seconds/age_to_seconds.adb
@@ -1,5 +1,5 @@
-with Ada.Text_IO, Ada.Integer_Text_IO, Ada.Calendar;
-use Ada.Text_IO, Ada.Integer_Text_IO, Ada.Calendar;
+with Ada.Text_IO, Ada.Integer_Text_IO, Ada.Calendar, Ada.Calendar.Arithmetic;
+use Ada.Text_IO, Ada.Integer_Text_IO, Ada.Calendar, Ada.Calendar.Arithmetic;
 
 procedure Age_To_Seconds is
 
@@ -16,14 +16,30 @@ procedure Age_To_Seconds is
 			Item => Month, Last => Index);
 		Get(From => Input(Index + 2..Input'Last),
 			Item => Day, Last => Index);
-		return Time_Of(Year => Year, Month => Month, Day => Day, Seconds => 0.0);
+		return Time_Of(Year => Year, Month => Month, Day => Day,
+			Seconds => 0.0);
 	end String_To_Time;
 
 	Input : String(1..10);
-	Birthday : Time;
+	Birthday, Now : Time;
+	Diff_Days : Day_Count;
+	Diff_Seconds : Duration;
+	Diff_LeapSeconds : Leap_Seconds_Count;
+	Total_Seconds : Integer;
 begin
 	Put_Line("Please enter your birthday in the format YYYY-MM-DD.");
 	Get(Input);
 	Skip_Line;
 	Birthday := String_To_Time(Input);
+	Now := Clock;
+	Difference(Now, Birthday, Diff_Days, Diff_Seconds, Diff_LeapSeconds);
+
+	-- This makes some really dumb assumptions like 30 days per month,
+	-- but it's ok for our purposes.
+	Total_Seconds :=  Integer(Diff_Days * 24 * 60 * 60)
+					+ Integer(Diff_Seconds)
+					+ Integer(Diff_LeapSeconds);
+	Put("It has been roughly ");
+	Put(Total_Seconds);
+	Put(" seconds since you were born.");
 end Age_To_Seconds;
